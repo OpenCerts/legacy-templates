2424340307e629bf6ffc0aef344a04bdff8b5f88
ccewuz <ccewuz@nus.edu.sg>
2019-08-02 16:53:29 +0800

    20190802 nus templates batch 1

---
+++ src/components/CertificateTemplates/tlds/sg/edu/nus/NUS-K1-2019/integration.spec.js
@@ -0,0 +1,34 @@
+import { Selector } from "testcafe";
+
+fixture("National University of Singapore").page`http://localhost:3000`;
+
+const Certificate = "./sample.opencert";
+
+const TemplateTabList = Selector("#template-tabs-list");
+const RenderedCertificate = Selector("#rendered-certificate");
+
+const validateTextContent = async (t, component, texts) =>
+  texts.reduce(
+    async (prev, curr) => t.expect(component.textContent).contains(curr),
+    Promise.resolve()
+  );
+
+test("NUS-K1-2019 degree scroll is rendered correctly", async t => {
+  // Uploads certificate via dropzone
+  await t.setFilesToUpload("input[type=file]", [Certificate]);
+
+  // Certificate tabs rendered
+  await t.expect(TemplateTabList.textContent).contains("Certificate");
+
+  // Certificate tab content
+  await validateTextContent(t, RenderedCertificate, [
+    "NATIONAL UNIVERSITY",
+    "OF SINGAPORE",
+    "BACHELOR OF ENGINEERING",
+    "PASS",
+    "MECHANICAL ENGINEERING (HONS)",
+    "30 June 2013",
+    "Chair, Board of Trustees",
+    "President"
+  ]);
+});
