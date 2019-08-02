92080fdd56df8a6955585c2a1dda93d9e0a72243
ccewuz <ccewuz@nus.edu.sg>
2019-08-02 16:53:29 +0800

    20190802 nus templates batch 1

--- src/components/CertificateTemplates/tlds/sg/edu/nus/common/index.js
+++ src/components/CertificateTemplates/tlds/sg/edu/nus/common/index.js
@@ -2,7 +2,10 @@ export * from "./routines";
 export { storeAddresses } from "./configure";
 export { NUS_LOGO } from "./imgNUSLogo";
 export { NUS_SEAL } from "./imgNUSSeal";
+<<<<<<< HEAD
 export { NUS_CHN_NAME } from "./imgNUSChineseName";
+=======
+>>>>>>> 20190802 nus templates batch 1
 export { DUKE_LOGO } from "./imgDUKELogo";
 export { DUKE_SEAL } from "./imgDUKESeal";
 export { NUS_YALENUS_LOGO } from "./imgNUSYaleNUSLogo";
@@ -12,10 +15,13 @@ export { ICL_LOGO } from "./imgICLLogo";
 export { ICL_SEAL } from "./imgICLSeal";
 export { JHU_LOGO } from "./imgJHULogo";
 export { UNC_SEAL } from "./imgUNCSeal";
+<<<<<<< HEAD
 export { UM_LOGO } from "./imgUMLogo";
 export { DTU_LOGO } from "./imgDTULogo";
 export { CAAS_LOGO } from "./imgCAASLogo";
 export { MPAS_LOGO } from "./imgMPASLogo";
+=======
+>>>>>>> 20190802 nus templates batch 1
 export { NUS_TS_BACKIMG } from "./imgNUSTsBack";
 export { NUS_TS_BACKIMG_YALE } from "./imgNUSTsBackYale";
 export { NUS_TS_LEGEND } from "./imgNUSTsLegend";
