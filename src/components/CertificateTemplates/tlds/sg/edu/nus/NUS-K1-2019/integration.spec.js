adbf07ad4e6ee6cabe105c7d7f6f1c70542096d3
ccewuz <ccewuz@nus.edu.sg>
2019-08-22 11:09:20 +0800

    20190822 updated framework

--- src/components/CertificateTemplates/tlds/sg/edu/nus/NUS-K1-2019/integration.spec.js
+++ src/components/CertificateTemplates/tlds/sg/edu/nus/NUS-K1-2019/integration.spec.js
@@ -6,6 +6,7 @@ import { getData } from "@govtechsg/open-attestation";
 fixture("National University of Singapore").page`http://localhost:3000`;
 
 const Certificate = "./sample.opencert";
+
 const RenderedCertificate = Selector("#rendered-certificate");
 
 const validateTextContent = async (t, component, texts) =>
@@ -14,7 +15,7 @@ const validateTextContent = async (t, component, texts) =>
     Promise.resolve()
   );
 
-test("NUS-K1-2019 degree scroll is rendered correctly", async t => {
+test("NUS-K1-2019 certificate is rendered correctly", async t => {
   // Inject javascript and execute window.opencerts.renderDocument
   const certificateContent = getData(
     JSON.parse(readFileSync(join(__dirname, Certificate)).toString())
@@ -29,32 +30,51 @@ test("NUS-K1-2019 degree scroll is rendered correctly", async t => {
   await t
     .expect(templates)
     .eql([
-      { id: "degree", label: "Certificate", template: undefined },
+      { id: "certificate", label: "Certificate", template: undefined },
       { id: "transcript", label: "Transcript", template: undefined }
     ]);
 
-  // Certificate tab content
   await validateTextContent(t, RenderedCertificate, [
-    "NATIONAL UNIVERSITY",
+    "NATIONAL",
+    "UNIVERSITY",
     "OF SINGAPORE",
-    "BACHELOR OF ENGINEERING",
-    "PASS",
-    "MECHANICAL ENGINEERING (HONS)",
-    "30 June 2013",
-    "Chair, Board of Trustees",
-    "President"
+    "A0058514E, NAME",
+    "Bachelor",
+    "of",
+    "Business",
+    "Administration",
+    "30 June 2013"
   ]);
-
-  // Transcript tab content
-  await t.eval(() => window.opencerts.selectTemplateTab(1));
   await validateTextContent(t, RenderedCertificate, [
-    "STUDENT NO:",
-    "A0056627Y",
-    "BACHELOR OF ENGINEERING (MECHANICAL ENGINEERING)",
+    "A0058514E, NAME",
+    "A0058514E",
+    "01/01/1905",
+    "20/08/2019",
+    "BACHELOR OF BUSINESS ADMINISTRATION",
     "COMPLETED PROGRAMME",
-    "ACADEMIC YEAR 2009/2010 SEMESTER 1",
-    "CUMULATIVE AVERAGE POINT :",
-    "CONFERRED/AWARDED THE DEGREE(S)/DIPLOMA(S) OF:",
-    "CONFERMENT DATE:"
+    "2009/2010 SEMESTER 1",
+    "ACC1002",
+    "FINANCIAL ACCOUNTING",
+    "2009/2010 SEMESTER 2",
+    "BSP1005",
+    "MANAGERIAL ECONOMICS",
+    "2010/2011 SEMESTER 1",
+    "ACC1006",
+    "ACCOUNTING INFORMATION SYSTEMS",
+    "2010/2011 SEMESTER 2",
+    "ACC2002",
+    "MANAGERIAL ACCOUNTING",
+    "2011/2012 SEMESTER 1",
+    "BSP2005",
+    "ASIA PACIFIC BUSINESS ENVIRONMENT",
+    "2011/2012 SEMESTER 2",
+    "BSP3001C",
+    "BUSINESS POLICY AND STRATEGY",
+    "2012/2013 SEMESTER 1",
+    "CH2243",
+    "CHINESE IN SOUTHEAST ASIA",
+    "2012/2013 SEMESTER 2",
+    "CH3226",
+    "MODERN CHINESE LITERATURE"
   ]);
 });
--- src/components/CertificateTemplates/tlds/sg/edu/nus/NUSTS-GENERAL-2019/integration.spec.js
+++ src/components/CertificateTemplates/tlds/sg/edu/nus/NUSTS-GENERAL-2019/integration.spec.js
@@ -6,6 +6,7 @@ import { getData } from "@govtechsg/open-attestation";
 fixture("National University of Singapore").page`http://localhost:3000`;
 
 const Certificate = "./sample.opencert";
+
 const RenderedCertificate = Selector("#rendered-certificate");
 
 const validateTextContent = async (t, component, texts) =>
@@ -14,7 +15,7 @@ const validateTextContent = async (t, component, texts) =>
     Promise.resolve()
   );
 
-test("NUSTS-GENERAL-2019 transcript is rendered correctly", async t => {
+test("NUSTS-GENERAL-2019 certificate is rendered correctly", async t => {
   // Inject javascript and execute window.opencerts.renderDocument
   const certificateContent = getData(
     JSON.parse(readFileSync(join(__dirname, Certificate)).toString())
@@ -30,12 +31,36 @@ test("NUSTS-GENERAL-2019 transcript is rendered correctly", async t => {
     .expect(templates)
     .eql([{ id: "transcript", label: "Transcript", template: undefined }]);
 
-  // Certificate tab content
   await validateTextContent(t, RenderedCertificate, [
+    "A0056627Y, NAME",
     "A0056627Y",
+    "01/01/1905",
+    "11/07/2019",
     "BACHELOR OF ENGINEERING (MECHANICAL ENGINEERING)",
-    "ME2103",
-    "Engineering Visualization & Modeling",
-    "30/06/2013"
+    "COMPLETED PROGRAMME",
+    "2009/2010 SEMESTER 1",
+    "MA1301",
+    "INTRODUCTORY MATHEMATICS",
+    "2009/2010 SEMESTER 2",
+    "MA1505",
+    "MATHEMATICS I",
+    "2010/2011 SEMESTER 1",
+    "MA1505",
+    "MATHEMATICS I",
+    "2010/2011 SEMESTER 2",
+    "ME2114",
+    "MECHANICS OF MATERIALS II",
+    "2011/2012 SEMESTER 1",
+    "MA1505",
+    "MATHEMATICS I",
+    "2011/2012 SEMESTER 2",
+    "EG2401",
+    "ENGINEERING PROFESSIONALISM",
+    "2012/2013 SEMESTER 1",
+    "GEK1521",
+    "PHYSICS IN THE LIFE SCIENCES",
+    "2012/2013 SEMESTER 2",
+    "LAK1201",
+    "KOREAN 1"
   ]);
 });
