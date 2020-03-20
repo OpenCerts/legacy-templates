6a284cdd113e19685dc68347f1f5d9c2fccdc0d0
jacelee2000 <jace_lee2000@yahoo.com>
2020-03-20 09:44:09 +0800

    delete common/approveaddress.js file change
    there is additional address variable for testing purpose.

--- src/components/CertificateTemplates/tlds/sg/edu/tp/common/approvedAddresses.js
+++
@@ -1,8 +0,0 @@
-const ApprovedAddresses = [
-  "0xe825e69383a57eB686Fc7b8c455Dd982bB9680f6", // ROPSTEN: Temasek Polytechnic
-  "0x4d05f843718da742b4229938f972a3eb969c25ce", // Temasek Polytechnic
-  "0xC573b7F751f88aFD69F7f9b11ec1E76efC47E198" // Temasek Polytechnic - Jace
-  
-];
-
-export default ApprovedAddresses;
