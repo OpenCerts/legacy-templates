30b7e6a88cff5dbceefafaf92f3d7e5d4064aea3
ccewuz <47135828+ccewuz@users.noreply.github.com>
2021-04-06 13:42:28 +0800

    Nus templates new (#113)

--- src/components/CertificateTemplates/tlds/sg/edu/nus/NUS-DUKEPHD-2020/degree.js
+++ src/components/CertificateTemplates/tlds/sg/edu/nus/NUS-DUKEPHD-2020/degree.js
@@ -13,7 +13,7 @@ const getDataFeeder = dataSource => {
   // logo is default
   dataFeeder.studentName = dataSource.recipient.name.toUpperCase();
   dataFeeder.postNameText =
-    "having fulfilled the requirements prescribed\nby the University and the Duke-NUS Medical School,\nconferred the degree of";
+    "having fulfilled the requirements prescribed\nby the University and the Duke-NUS Medical School,\nwas conferred the degree of";
   dataFeeder.degreeCode = dataSource.additionalData.degreeScroll[0].degreeCode;
   dataFeeder.degreeTitle =
     dataSource.additionalData.degreeScroll[0].degreeTitle;
