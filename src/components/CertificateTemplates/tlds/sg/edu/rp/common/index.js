c691771ada8167481fddc8d786c5cc12b78c3f48
Tng Zi Xiang <tngzixiang@gmail.com>
2023-06-16 09:00:17 +0800

    feat: add rp goerli address

--- src/components/CertificateTemplates/tlds/sg/edu/rp/common/index.js
+++ src/components/CertificateTemplates/tlds/sg/edu/rp/common/index.js
@@ -2,5 +2,6 @@ export * from "./images";
 
 export const approvedAddresses = [
   "0x061aFcFF60b90E0F633F8ef157e01BaB9b2FfDD3",
-  "0x18270bA6dA0380a2cbC705bc6C0AD6651282bD14"
+  "0x18270bA6dA0380a2cbC705bc6C0AD6651282bD14",
+  "0x5AA6bF8F748cD2D8f1949eD328B2bEa61CE2B6d7",
 ];
