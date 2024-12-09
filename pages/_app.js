0006f6030c72ec43a1206ea36ddd657c676066ff
Kyle Huang Junyuan <kyle@hjunyuan.com>
2024-12-09 11:55:30 +0800

    fix: remove deprecated Container component

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import App, { Container } from "next/app";
+import App from "next/app";
 import React from "react";
 
 class MyApp extends App {
@@ -14,11 +14,7 @@ class MyApp extends App {
 
   render() {
     const { Component, pageProps } = this.props;
-    return (
-      <Container>
-        <Component {...pageProps} />
-      </Container>
-    );
+    return <Component {...pageProps} />;
   }
 }
 
