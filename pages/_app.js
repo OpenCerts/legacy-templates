b041661f6ea51831f591918a2aa2c2a5ec5733bb
Kyle Huang Junyuan <kyle@hjunyuan.com>
2024-12-09 14:48:39 +0800

    fix: move viewport meta tags into _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import App from "next/app";
+import Head from "next/head";
 import React from "react";
 
 class MyApp extends App {
@@ -14,7 +15,14 @@ class MyApp extends App {
 
   render() {
     const { Component, pageProps } = this.props;
-    return <Component {...pageProps} />;
+    return (
+      <>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <Component {...pageProps} />
+      </>
+    );
   }
 }
 
